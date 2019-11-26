b46380d708bd302a30038b576ba55a0115a59090
szdytom <szdytom@163.com>
2019-11-26 18:54:02 +0800

    add graph
    Signed-off-by: szdytom <szdytom@163.com>

---
+++ graph.h
@@ -0,0 +1,42 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include "random.h"
+
+namespace catu {
+
+template<class T>
+class Edge {
+public:
+	int x, y;
+	T w;
+
+	Edge();
+
+	Edge(int x, int y, T w) {
+		this->x = x;
+		this->y = y;
+		this->w = w;
+	}
+
+	~Edge();	
+};
+
+template<class T> 
+class Graph {
+public:
+	std::vector<Edge> e;
+	Graph();
+	~Graph() {
+		e.clear();
+	}
+
+	void graph(int n, int m, bool self_loop = 1, XYpair<T> weight_limit = XYpair<T>(1, 1), bool reapt_edge = 1) {
+
+	}
+};
+
+}
+
+#endif
