5691b1d23fe0af683e1f99c924cde0b354cf0c8d
zhangtianli <zhangtianli2006@163.com>
2019-11-26 11:15:43 +0800

    更新文件 maze.cpp

--- maze.cpp
+++ maze.cpp
@@ -1,17 +1,44 @@
+#include <vector>
 #include "random.h"
+
+namespace catu
+{
 
 template <typename T>
 class maze
 {
+private:
+	void dfs()
+	{
+		
+	}
 public:
-	std::vector<std::vector<T> > __maze_two;
+	std::vector<std::vector<T> > maze;
 	unsigned int size()
 	{
-		unsigned int __res = 0;
-		for (unsigned int i = 0; i < __maze_two.size(); i ++) __res += __maze_two[i].size();
-		return __res;
+		unsigned int res = 0;
+		for (unsigned int i = 0; i < maze.size(); i ++) res += maze[i].size();
+		return res;
 	}
-	void __make_rand(int l, int r)
+	void build_rand(int l, int r, std::vector<T> able)
+	{
+		catu::rand_init();
+		for (int i = 0; i < l; i ++)
+		{
+			std::vector<T> tmp;
+			for (int j = 0; j < r; j ++)
+				tmp.push_back(able[catu::rrange(0, able.size() - 1)]);
+			maze.push_back(tmp);
+		}
+	}
+	void build_road(int start_x, int start_y, int end_x, int end_y, T road, T wall, std::vector<T> able)
 	{
 	}
+	void clear()
+	{
+		for (int i = 0; i < maze.size(); i ++)
+			maze[i].clear();
+	}
 };
+
+}
