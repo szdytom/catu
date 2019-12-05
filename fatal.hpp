bc1efd42519dbdf7c5b5b54ecd07b4028b0b5de3
zhangtianli <zhangtianli2006@163.com>
2019-12-05 18:52:40 +0800

    更新文件 fatal.hpp

--- fatal.hpp
+++ fatal.hpp
@@ -3,15 +3,34 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <vector>
 
 namespace catu
 {
+    int read_type(std::vector<char> tt)
+    {
+        
+    }
     void errorf(char* type, ...)
     {
-        for (int i = 0; i < strlen(type); i ++)
+        int len = strlen(type);
+        std::vector<char> tt;
+        std::vector<int> input_stream;
+        for (int i = 0; i < len; i ++)
         {
-            
+            int k = i + 1;
+            while (k < len)
+            {
+                if (type[k] == '%')
+                {
+                    i = k;
+                    break;
+                }
+                tt.push_back(type[k]);
+            }
         }
+        input_stream.push_back(read_type(tt));
+        tt.clear();
     }
 }
 
