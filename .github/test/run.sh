ba02e453d9fce2d6d9106c662ebe01a0f87f9cb9
szdytom <szdytom@163.com>
2020-01-16 14:52:22 +0800

    create run.sh

---
+++ .github/test/run.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/sh
+
+echo Compile Start
+for code in $(echo *.cpp); do
+	g++ -std=c++11 -Wall -Wextra $code -o $code.out
+	if [ $? != 0 ]; then
+		echo $code Compile OK
+	else
+		echo $code Compile Error
+		exit 1
+	fi
+done
+
+echo Compile OK
+echo Running the test code
+for code in $(echo *.out); do
+	./$code
+	if [ $? != 0 ]; then
+		echo Test $code OK!
+	else
+		echo Test $code FAIL!
+		exit 1
+	fi
+done
+
+echo All Test Completed
